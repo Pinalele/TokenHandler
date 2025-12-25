8458650a2e0132913ed287b5094f963a2653bfaf
marcio <marcio.pinela@gmail.com>
2025-12-25 22:13:26 +0000

    First Commit

---
+++ src/main/java/com/mpi/tokenhandler/TokenHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.mpi.tokenhandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenHandlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TokenHandlerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mpi/tokenhandler/TokenHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpi.tokenhandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenHandlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
